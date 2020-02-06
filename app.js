021636757f7cabd7286a40fe185272f3e3c1c8a7
Agata <agatajalowska@gmail.com>
2020-02-06 21:00:20 +0100

    basic structure

---
+++ app.js
@@ -0,0 +1,15 @@
+var counter = 0;
+
+function uplikes(){
+    counter = counter + 1;
+
+    document.getElementById("likes").innerHTML = counter + " likes";
+}
+
+
+function downlikes(){
+    counter = counter- 1;
+
+    document.getElementById("likes").innerHTML = counter + " likes";
+}
+
